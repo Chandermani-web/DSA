4c8260b37cebdd0dc9b793a73add6d103131bc3b
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2026-03-25 19:51:46 +0530

    Add 07Tree/AVLtree/script.js

---
+++ 07Tree/AVLtree/script.js
@@ -0,0 +1,19 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+}
+
+class AVLTree{
+    constructor(props) {
+        super(props);    
+        this.root = null;
+    }
+
+    insert(value){
+         
+    }
+}
